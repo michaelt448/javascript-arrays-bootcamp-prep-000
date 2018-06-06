b115850ceab926ffbdf0cd4f476da67df63d1a41
michael <michaeltilman77@gmail.com>
2018-06-06 00:15:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,28 @@ function addElementToBeginningOfArray(array, element){
   return [element,...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.
+  return array.unshift(element)
+}
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element)
+}
+function accessElementInArray(array, index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array){
+  array.slice(-1)
 }
